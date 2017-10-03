e757c78f25d999db1157e92a0e9cda018cb766e6
Chris Ranni <cranni@marx.tech>
2017-10-03 03:03:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+let breakOut = function(array,changeValue,stopValue){
+  for (let i=0;i<array.length;i++) {
+    if (array[i]==stopValue){
+      return array
+    } else {
+      array[i]=changeValue
+    }
+  }
+  return array
+}
+
+let keepGoing = function(array,changeValue,skipValue){
+  for (let i=0;i<array.length;i++) {
+    if (array[i] !== skipValue) {
+      array[i] = changeValue
+    }
+  }
+  return array
+}
+
+function findBy(array, findFn) {
+  for (let i = 0, l = array.length; i < l; i++) {
+    if (findFn(array[i])) {
+      return array[i]
+    }
+  }
+
+  return null
+}
